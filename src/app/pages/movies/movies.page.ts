3048d5e549574e97fe7c0207779072885869627a
siapa? <dimasahmadrizal@gmail.com>
2019-04-13 08:58:16 +0700

    view Movie

--- src/app/pages/movies/movies.page.ts
+++ src/app/pages/movies/movies.page.ts
@@ -1,4 +1,6 @@
+import { SearchType, MovieService } from './../../services/movie.service';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-movies',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoviesPage implements OnInit {
 
-  constructor() { }
+  result: Observable<any>;
+  searchTerm = '';
+  type: SearchType = SearchType.all;
+
+
+  constructor(private movieService: MovieService) { }
 
   ngOnInit() {
+  }
+
+  searchChanged() {
+    this.result = this.movieService.searchData(this.searchTerm, this.type);
   }
 
 }
